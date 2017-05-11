f8009be4ed231a496028d48f9d58b53a88b2e17b
Timothy McKee <timothy.l.mckee@gmail.com>
2017-05-10 21:29:45 -0400

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
 
-    <title>WorkMeNow</title>
+    <title>WorkMeNow Test</title>
     
     <!-- Load Fonts -->
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" />
